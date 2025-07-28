29b2e02c99b353e294339db172a929a14cde6d9a
aryfareq <121553546+aryfareq@users.noreply.github.com>
2025-07-28 14:33:35 +0300

    Created roomTypes.js
    instead of putting it in the DB, i thought we should just store the room types data in a JS file

---
+++ auisdormsystem/src/assets/roomTypes.js
@@ -0,0 +1,42 @@
+const roomTypes = [
+    {
+        TypeName: "Economy (6 Person)",
+        FallSpringPrice: {
+            UG: 835000,
+            APP: 685000,
+            MasterAlumni: 385000,
+        },
+        WinterSummerPrice: {
+            FourWeeks: 225000,
+            SevenWeeks: 390000,
+            NineWeeks: 500000,
+            TwelveWeeks: 680000,
+        }
+    }, {
+        TypeName: "Standard (4 Person)",
+        FallSpringPrice: {
+            UG: 1000000,
+            APP: 820000,
+            MasterAlumni: 469000,
+        },
+        WinterSummerPrice: {
+            FourWeeks: 275000,
+            SevenWeeks: 465000,
+            NineWeeks: 600000,
+            TwelveWeeks: 820000,
+        }
+    }, {
+        TypeName: "Private (1 Person)",
+        FallSpringPrice: {
+            UG: 1670000,
+            APP: 1390000,
+            MasterAlumni: 765000,
+        },
+        WinterSummerPrice: {
+            FourWeeks: 445000,
+            SevenWeeks: 780000,
+            NineWeeks: 1000000,
+            TwelveWeeks: 1390000,
+        }
+    }
+];
