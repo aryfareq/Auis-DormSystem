521f9968341ce8d79bec3667c717e0b841b45d0f
aryfareq <121553546+aryfareq@users.noreply.github.com>
2025-07-31 17:40:26 +0300

    Created Admin Navbar

---
+++ auisdormsystem/src/pages/admin/adminPage.js
@@ -0,0 +1,10 @@
+import AdminNav from '../../components/adminNav';
+function adminPage() {
+  return (
+    <div>
+        <AdminNav />
+      {/* Add more admin functionalities here */}
+    </div>
+  );
+}
+export default adminPage;
