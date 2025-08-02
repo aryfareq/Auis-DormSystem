b70732cc9747e72ccea08eee2abe5fe287322187
aryfareq <121553546+aryfareq@users.noreply.github.com>
2025-08-02 18:52:57 +0300

    Created a makeshift array for the rooms, instead of a database

---
+++ auisdormsystem/src/assets/apartments.js
@@ -0,0 +1,51 @@
+const apartments = [
+  {
+    type: "standard",
+    floors: [0, 1],
+    apartments: [
+      // Floor 0
+      { floor: 0, apartmentNumber: 1, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 0, apartmentNumber: 2, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 0, apartmentNumber: 3, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 0, apartmentNumber: 4, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 0, apartmentNumber: 5, rooms: [{ room: 1 }, { room: 2 }] },
+
+      // Floor 1
+      { floor: 1, apartmentNumber: 1, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 1, apartmentNumber: 2, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 1, apartmentNumber: 3, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 1, apartmentNumber: 4, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 1, apartmentNumber: 5, rooms: [{ room: 1 }, { room: 2 }] }
+    ]
+  },
+  {
+    type: "economy",
+    floors: [2, 3],
+    apartments: [
+      // Floor 2
+      { floor: 2, apartmentNumber: 1, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 2, apartmentNumber: 2, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 2, apartmentNumber: 3, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 2, apartmentNumber: 4, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 2, apartmentNumber: 5, rooms: [{ room: 1 }, { room: 2 }] },
+
+      // Floor 3
+      { floor: 3, apartmentNumber: 1, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 3, apartmentNumber: 2, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 3, apartmentNumber: 3, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 3, apartmentNumber: 4, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 3, apartmentNumber: 5, rooms: [{ room: 1 }, { room: 2 }] }
+    ]
+  },
+  {
+    type: "private",
+    floors: [4],
+    apartments: [
+      { floor: 4, apartmentNumber: 1, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 4, apartmentNumber: 2, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 4, apartmentNumber: 3, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 4, apartmentNumber: 4, rooms: [{ room: 1 }, { room: 2 }] },
+      { floor: 4, apartmentNumber: 5, rooms: [{ room: 1 }, { room: 2 }] }
+    ]
+  }
+];
